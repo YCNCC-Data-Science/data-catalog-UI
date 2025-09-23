111c1b54a32cb7db1bd945990c6ca197db601129
Luke Gloege <lukegloege@gmail.com>
2025-09-23 19:24:16 -0400

    fix: change Feedstock type to Dataset

--- app/dataset/[slug]/client-dataset-page.tsx
+++ app/dataset/[slug]/client-dataset-page.tsx
@@ -3,17 +3,14 @@
 import { SectionDivider } from "@/components/divider";
 import React from "react";
 import { Container } from "theme-ui";
-
-import { Feedstock } from "@/types/types";
+import { Dataset } from "@/types/types";
 import { DatasetDescription } from "./description";
 import { DatasetDetails } from "./details";
 import { DatasetHeader } from "./header";
 import { Manuscripts } from "./manuscripts";
 
-//import { FeedstockStores } from './stores'
-
 interface ClientFeedstockPageProps {
-  feedstock: Feedstock;
+  feedstock: Dataset;
 }
 
 const ClientDatasetPage: React.FC<ClientFeedstockPageProps> = ({
